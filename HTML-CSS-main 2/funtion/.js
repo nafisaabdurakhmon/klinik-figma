7c035358d3b906a2b4f4b3fc4ff29bf94334a8c8
Abdurakhmon <abdurakhmon@192.168.0.114>
2026-02-18 18:37:14 +0500

    first commit

---
+++ HTML-CSS-main 2/funtion/.js
@@ -0,0 +1,21 @@
+/*function checkPassword(password){
+    let safepassword = 1991;
+    if(password === safepassword){
+        console.log("phone is unlocked")
+     }
+     else{
+        console.log('phone is loked')
+         }
+}
+
+
+
+checkPassword(1991)*/
+
+
+function canculetDiscound(price,discound){
+    return price - (price * discound / 100)
+}
+console.log(canculetDiscound(100,20))
+
+
